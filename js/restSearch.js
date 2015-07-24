e3d0458b6c545a33ae3af93bea8e7f1978fd16f6
Ruther Bergonia <ruther.bergonia@gmail.com>
2015-07-25 01:38:50 +0800

    3

---
+++ js/restSearch.js
@@ -0,0 +1,30 @@
+// Let's create a pollyfill for Object.create
+if ( typeof Object.create !== 'function') {
+    
+    Object.create = function( obj ){
+        function F() {};
+        F.prototype = obj;
+        return new F();
+    };
+}
+( function( $, window, document, undefined ) {
+    
+  
+
+
+    $.fn.restSearch = function( options ) {
+        
+        return this.each(function() {
+         
+        });
+        
+
+    };
+
+
+    // Set plugin options here.
+    $.fn.restSearch.options = {
+       
+    };
+
+}( jQuery, window, document ));
